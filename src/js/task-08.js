fb6b95faa0ce8e85d04585139e43472bc7c80b87
Ivan1108 <ivan.horianckiy@gmail.com>
2022-09-22 18:19:04 +0300

    Дз6

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -7,7 +7,15 @@ refs.form.addEventListener('submit',onFormSubmit);
 function onFormSubmit(event){
     event.preventDefault();
     const formElements= event.currentTarget.elements;
-
-    const mail = formElements.email.value;
-    const password = formElements.password.value;
+    var mail = "";
+    var password = "";
+    if(formElements.password.value === ""|| formElements.email.value === ""){
+        alert("Все поля должны быть заполнены")
+    }else{
+        mail = formElements.email.value;
+        password = formElements.password.value;
+    }
+    
+    console.log(mail,password)
+   
 }
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const color = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+color.addEventListener('click', change_color);
+function change_color(getRandomHexColor){
+  span.innerHTML = getRandomHexColor;
+  color.style.background = getRandomHexColor;
+  console.log(getRandomHexColor);
+}
+
